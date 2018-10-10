2efff76ca382ddd0aef05b5e624370fc97c89999
raquelita <rvmederos@gmail.com>
2018-10-10 22:01:19 +0200

    Add proper simulation method

--- popup.js
+++ popup.js
@@ -1,22 +1,12 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
+let applyDiscount = document.getElementById('applyDiscount');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  let priceWithDiscount = "35"; // TODO harcoded text for testing purposes 
+applyDiscount.onclick = function(element) {
+  console.log("testing")
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
-        {code: 'document.getElementsByClassName("CTASection__price-2bc7h price")[0].textContent = "' + priceWithDiscount + '";'});
+        { file: "/content-script.js" })
   });
 };
